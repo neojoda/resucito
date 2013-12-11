95c4a16f2e01f3b2079071b20f0f770b1cf27555
Joan B. Gimenez <neojoda@yahoo.es>
2013-12-11 14:48:22 -0700

    CakePHP basic installation done

--- Config/core.php
+++ Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '7akNx4yVTtxE*4mt,o5#ACgNeo4@U%,I5cAP!6dHy|Lzo&B8dK1^9ut6s!x2');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '49139576275502700229885896610193279323615473792597');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
